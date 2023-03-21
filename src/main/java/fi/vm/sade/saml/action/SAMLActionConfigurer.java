2ffba7109c81c84f064148925701e0a392004147
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2023-03-21 09:52:02 +0200

    KJHH-2281 add missing state transition for saml state

--- src/main/java/fi/vm/sade/saml/action/SAMLActionConfigurer.java
+++ src/main/java/fi/vm/sade/saml/action/SAMLActionConfigurer.java
@@ -11,6 +11,8 @@ import org.springframework.webflow.engine.DecisionState;
 import org.springframework.webflow.engine.Flow;
 import org.springframework.webflow.engine.builder.support.FlowBuilderServices;
 
+import static org.apereo.cas.gauth.web.flow.GoogleAuthenticatorMultifactorWebflowConfigurer.MFA_GAUTH_EVENT_ID;
+
 public class SAMLActionConfigurer extends AbstractCasWebflowConfigurer {
 
     private static final String STATE_ID_SAML_DECISION = "samlDecision";
@@ -40,6 +42,8 @@ public class SAMLActionConfigurer extends AbstractCasWebflowConfigurer {
         ActionState actionState = super.createActionState(loginFlow, STATE_ID_SAML_ACTION, this.samlAction);
         super.createTransitionForState(actionState, CasWebflowConstants.TRANSITION_ID_SUCCESS,
                 CasWebflowConstants.STATE_ID_CREATE_TICKET_GRANTING_TICKET);
+        super.createTransitionForState(actionState, MFA_GAUTH_EVENT_ID, MFA_GAUTH_EVENT_ID);
+
         super.createStateDefaultTransition(actionState, originalStartState);
         actionState.getExitActionList().add(super.createEvaluateAction(
                 CasWebflowConstants.ACTION_ID_CLEAR_WEBFLOW_CREDENTIALS));
